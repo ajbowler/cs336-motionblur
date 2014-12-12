b778d19c9e53fbb94b29a29f5637a7de54c18675
Andrew Bowler <ajbowler@iastate.edu>
2014-12-11 18:46:42 -0600

    Scene before blur implementation, decided not to use obj files cause they're too unpredictable when rendering

--- WWW/index.js
+++ WWW/index.js
@@ -56,7 +56,7 @@ function cameraControl(c, ch)
     c.translateY(100);
     return true;
   case 'f':
-    c.translateY(100);
+    c.translateY(-100);
     return true;
   case 'j':
     // need to do extrinsic rotation about world y axis, so multiply
@@ -140,25 +140,27 @@ function handleKeyPress(event)
 
 var imagePath = '../resources/images/';
 var images = [
-  imagePath + 'skybox_front.jpg',
-  imagePath + 'skybox_back.jpg',
-  imagePath + 'skybox_top.jpg',
-  imagePath + 'roads.jpg',
-  imagePath + 'skybox_right.jpg',
-  imagePath + 'skybox_left.jpg'
+  imagePath + 'px.png',
+  imagePath + 'nx.png',
+  imagePath + 'py.png',
+  imagePath + 'ny.png',
+  imagePath + 'pz.png',
+  imagePath + 'nz.png'
 ];
+
+var planeImage = imagePath + 'roads.jpg';
 
 function start() {
  window.onkeypress = handleKeyPress;
 
   var scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(45, 1.5, 0.1, 500000);
-  // camera.position.x = -180000;
-  camera.position.x = 0;
-  // camera.position.y = -100000;
-  camera.position.y = 0;
-  camera.position.z = 0;
-  camera.lookAt(new THREE.Vector3(40000, -160000, 0));
+  camera = new THREE.PerspectiveCamera(45, 1.5, 0.1, 100000);
+  camera.position.x = 19000;
+  camera.position.y = 9800;
+  camera.position.z = 1400;
+  camera.lookAt(new THREE.Vector3(-1000, -6000, 0));
+  var light = new THREE.AmbientLight( 0xB0B0B0 );
+  scene.add( light );
 
   var ourCanvas = document.getElementById('theCanvas');
   var renderer = new THREE.WebGLRenderer(
@@ -178,51 +180,79 @@ function start() {
     fragmentShader : skyboxShader.fragmentShader,
     vertexShader : skyboxShader.vertexShader,
     uniforms : skyboxShader.uniforms,
-    side : THREE.DoubleSide
+    side : THREE.BackSide
   });
 
-  var skyboxGeometry = new THREE.BoxGeometry(80000, 80000, 80000);
+  var skyboxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
 
   var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
   skybox.scale.set(5, 5, 5);
-
   scene.add(skybox);
+
+  var roadGeometry = new THREE.PlaneGeometry( 49500, 49500);
+  var roadMaterial = new THREE.MeshPhongMaterial({
+    map: THREE.ImageUtils.loadTexture(planeImage)
+  });
+
+  var road = new THREE.Mesh(roadGeometry, roadMaterial);
+  road.rotation.x -= 90 * Math.PI / 180;
+  road.position.y -= 10000;
+  scene.add(road);
 
   ///////////////////////////////////
   ///////////  OBJECTS  /////////////
   ///////////////////////////////////
 
-  var manager = new THREE.LoadingManager();
-  manager.onProgress = function(item, loaded, total) {
-  };
+  // Create a car
+  var carDummy = new THREE.Object3D();
 
-  var onProgress = function(xhr) {
-  };
+  var carGeometry = new THREE.BoxGeometry(1, 1, 1);
+  var carMaterial = new THREE.MeshPhongMaterial({
+    color : 0xcc0000,
+    ambient : 0xff0000,
+    specular : 0x050505,
+    shininess : 50
+  });
 
-  var onError = function(xhr) {
-  };
+  var car = new THREE.Mesh(carGeometry, carMaterial);
+  carDummy.add(car);
+  carDummy.position.set(20000, -8010, -6640);
+  carDummy.scale.set(2000, 3000, 6000);
 
-  var car1, car2;
-  var loader = new THREE.OBJMTLLoader();
-  loader.load('../resources/objects/L200-OBJ.obj', '../resources/objects/L200-OBJ.mtl', function(object){
-    object.position.x = 0;
-    object.position.y = 0;
-    object.position.z = 0;
-    scene.add(object);
-    car1 = object;
-  }, onProgress, onError);
+  carDummy.rotation.y += 90 * Math.PI / 180;
 
-  loader.load('../resources/objects/L200-OBJ.obj', '../resources/objects/L200-OBJ.mtl', function(object){
-    object.position.x = 300;
-    object.position.y = 0;
-    object.position.z = 0;
-    scene.add(object);
-    car2 = object;
-  }, onProgress, onError);
+  // Make a total of 4 cars.
+  var carDummy2 = carDummy.clone();
+  var carDummy3 = carDummy.clone();
+  var carDummy4 = carDummy.clone();
+
+  // Set their positions on the road.
+  carDummy2.position.set(20000, -8010, -15100);
+  carDummy3.position.set(-20000, -8010, 6640);
+  carDummy4.position.set(-20000, -8010, 15100);
+
+  scene.add(carDummy);
+  scene.add(carDummy2);
+  scene.add(carDummy3);
+  scene.add(carDummy4);
 
   var render = function()
   {
     requestAnimationFrame(render);
+
+    var increment = 300;
+    //var increment = document.getElementById('increment');
+    carDummy.position.x -= increment;
+    carDummy2.position.x -= increment;
+    carDummy3.position.x += increment;
+    carDummy4.position.x += increment;
+
+    if(carDummy.position.x < -30000) {
+      carDummy.position.set(20000, -8010, -6640);
+      carDummy2.position.set(20000, -8010, -15100);
+      carDummy3.position.set(-20000, -8010, 6640);
+      carDummy4.position.set(-20000, -8010, 15100);
+    }
 
     renderer.render(scene, camera);
   };
