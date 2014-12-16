c3424476b3df0ad2a3dfda8c67494ea216a23f39
Andrew Bowler <ajbowler@iastate.edu>
2014-12-15 18:15:19 -0600

    Code cleanup

--- WWW/index.js
+++ WWW/index.js
@@ -1,14 +1,21 @@
+/* jslint sub: true */
+/* jslint browser: true*/
+/* global THREE */
+/* global dat: true*/
+
 var container;
 var g;
-var camera, controls, scene, projector, renderer;
+var camera, controls, scene, renderer;
 var car;
 var objects = [], plane, sphere;
 var composer, pass, composer2, pass2;
 var mesh;
 var clock = new THREE.Clock();
-var lon = lat = 0, position = { x: 0, y: 0 }, isUserInteracting = false;
-var keys = { up: false, left: false, right: false, down: false, forward: false, back: false };
-var fov = nfov = 70;
+var lon = 0;
+var lat = 0, position = {x: 0, y: 0}, isUserInteracting = false;
+var keys = {up: false, left: false, right: false, down: false, forward: false, back: false};
+var fov = 70;
+var nfov = 70;
 
 var mCurrent = new THREE.Matrix4();
 var mPrev = new THREE.Matrix4();
@@ -16,14 +23,14 @@ var tmpArray = new THREE.Matrix4();
 var camTranslateSpeed = new THREE.Vector3();
 var prevCamPos = new THREE.Vector3();
 
-var mouse = new THREE.Vector2(),
-offset = new THREE.Vector3(),
-INTERSECTED, SELECTED;
+var mouse = new THREE.Vector2();
+var offset = new THREE.Vector3();
+var INTERSECTED, SELECTED;
 
 var depthMaterial;
-var meshMaterial = new THREE.MeshPhongMaterial( { color: 0x806040, specular: 0xffffff, specularity: 10, shading: THREE.FlatShading });
-var meshMaterial2 = new THREE.MeshBasicMaterial( { color: 0xffffff, emissive: 0xffffff });
-var sphereMaterial = new THREE.MeshNormalMaterial( { color: 0xff00ff, specular: 0x806040, specularity: 10, shading: THREE.SmoothShading });
+var meshMaterial = new THREE.MeshPhongMaterial({color: 0x806040, specular: 0xffffff, specularity: 10, shading: THREE.FlatShading});
+var meshMaterial2 = new THREE.MeshBasicMaterial({color: 0xffffff, emissive: 0xffffff});
+var sphereMaterial = new THREE.MeshNormalMaterial({color: 0xff00ff, specular: 0x806040, specularity: 10, shading: THREE.SmoothShading});
 
 var imagePath = '../resources/images/';
 var images = [
@@ -37,7 +44,7 @@ var images = [
 
 var planeImage = imagePath + 'roads.jpg';
 
-var Params = function() { 
+var Params = function() {
   this.blur = 1;
   this.fps = 60;
   this.carSpeed = 20;
@@ -49,13 +56,13 @@ animate();
 
 function init() {
 
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
+  container = document.createElement('div');
+  document.body.appendChild(container);
 
-  camera = new THREE.PerspectiveCamera( fov, window.innerWidth / window.innerHeight, 1, 9000 );
-  camera.position.set( 10, 0, 0 );
+  camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 1, 9000);
+  camera.position.set(-50, 1300, 2250);
 
-  /*controls = new THREE.TrackballControls( camera );
+  /*controls = new THREE.TrackballControls(camera);
   controls.rotateSpeed = 1.0;
   controls.zoomSpeed = 1.2;
   controls.panSpeed = 0.8;
@@ -63,16 +70,16 @@ function init() {
   controls.noPan = false;
   controls.staticMoving = true;
   controls.dynamicDampingFactor = 0.3;*/
-  controls = new THREE.OrbitControls( camera );
+  controls = new THREE.OrbitControls(camera);
   controls.damping = 0.2;
   controls.keyPanSpeed = 700;
 
   scene = new THREE.Scene();
 
-  scene.add( new THREE.AmbientLight( 0x505050 ) );
+  scene.add(new THREE.AmbientLight(0x505050));
 
-  var light = new THREE.SpotLight( 0xffffff, 1.5 );
-  light.position.set( 0, 500, 2000 );
+  var light = new THREE.SpotLight(0xffffff, 1.5);
+  light.position.set(0, 500, 2000);
   light.castShadow = true;
 
   light.shadowCameraNear = 200;
@@ -85,8 +92,8 @@ function init() {
   light.shadowMapWidth = 2048;
   light.shadowMapHeight = 2048;
 
-  var light = new THREE.SpotLight( 0xffffff, 1.5 );
-  light.position.set( 500, -500, 2000 );
+  var light = new THREE.SpotLight(0xffffff, 1.5);
+  light.position.set(500, -500, 2000);
   light.castShadow = true;
 
   light.shadowCameraNear = 200;
@@ -99,17 +106,12 @@ function init() {
   light.shadowMapWidth = 2048;
   light.shadowMapHeight = 2048;
 
-  scene.add( light );
-
-  sphere = new THREE.Mesh( new THREE.SphereGeometry( 1500, 30, 30 ), sphereMaterial );
-  sphere.scale.x = -1;
-  //scene.add( sphere );
+  scene.add(light);
   
   var skyboxMap = THREE.ImageUtils.loadTextureCube(images);
   var skyboxShader = THREE.ShaderLib["cube"];
   skyboxShader.uniforms["tCube"].value = skyboxMap;
-  var skyboxMaterial = new THREE.ShaderMaterial(
-  {
+  var skyboxMaterial = new THREE.ShaderMaterial({
     fragmentShader : skyboxShader.fragmentShader,
     vertexShader : skyboxShader.vertexShader,
     uniforms : skyboxShader.uniforms,
@@ -122,7 +124,7 @@ function init() {
   skybox.scale.set(5, 5, 5);
   scene.add(skybox);
 
-  var roadGeometry = new THREE.PlaneGeometry( 6000, 6000);
+  var roadGeometry = new THREE.PlaneGeometry(6000, 6000);
   var roadMaterial = new THREE.MeshPhongMaterial({
     map: THREE.ImageUtils.loadTexture(planeImage)
   });
@@ -134,7 +136,7 @@ function init() {
 
   var s = 3;
   g = new THREE.Geometry();
-  var geometry = new THREE.BoxGeometry( 40, 40, 40 );
+  var geometry = new THREE.BoxGeometry(40, 40, 40);
 
   var carGeometry = new THREE.BoxGeometry(1, 1, 1);
 
@@ -146,85 +148,78 @@ function init() {
   });
 
   car = new THREE.Mesh(carGeometry, carMaterial);
-  car.position.set(800, -260, 640);
+  car.position.set(1500, -260, 640);
   car.scale.set(160, 300, 800);
 
   car.rotation.y += 90 * Math.PI / 180;
 
   car.updateMatrixWorld();
 
-  //THREE.GeometryUtils.merge( g, object );
-  g.merge( car.geometry, car.matrixWorld );
+  //THREE.GeometryUtils.merge(g, object);
+  g.merge(car.geometry, car.matrixWorld);
 
-  
-
-  depthMaterial = new THREE.ShaderMaterial( {
+  depthMaterial = new THREE.ShaderMaterial({
 
     uniforms: {
-      mNear: { type: 'f', value: camera.near },
-      mFar: { type: 'f', value: camera.far },
-      opacity: { type: 'f', value: 1 }
+      mNear: {type: 'f', value: camera.near},
+      mFar: {type: 'f', value: camera.far},
+      opacity: {type: 'f', value: 1}
     },
 
-    vertexShader: document.getElementById( 'vs-depthRender' ).textContent,
-    fragmentShader: document.getElementById( 'fs-depthRender' ).textContent
-  } );
+    vertexShader: document.getElementById('vs-depthRender').textContent,
+    fragmentShader: document.getElementById('fs-depthRender').textContent
+  });
 
-  mesh = new THREE.Mesh( g, depthMaterial );
+  mesh = new THREE.Mesh(g, depthMaterial);
   mesh.castShadow = true;
   mesh.receiveShadow = true;
-  scene.add( mesh );
+  scene.add(mesh);
   scene.add(car);
 
-  mesh2 = new THREE.Mesh( g, depthMaterial );
-  scene.add( mesh2 );
+  mesh2 = new THREE.Mesh(g, depthMaterial);
+  scene.add(mesh2);
 
-  plane = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.25, transparent: true, wireframe: true } ) );
-  plane.visible = false;
-  scene.add( plane );
 
-  projector = new THREE.Projector();
-
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  document.body.appendChild( renderer.domElement );
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  document.body.appendChild(renderer.domElement);
   renderer.sortObjects = false;
-  renderer.setClearColor( 0 );
+  renderer.setClearColor(0);
   renderer.shadowMapEnabled = true;
   renderer.shadowMapType = THREE.PCFShadowMap;
 
-  composer = new THREE.EffectComposer( renderer );
-  composer.addPass( new THREE.RenderPass( scene, camera ) );
+  composer = new THREE.EffectComposer(renderer);
+  composer.addPass(new THREE.RenderPass(scene, camera));
 
-  composer2 = new THREE.EffectComposer( renderer );
-  composer2.addPass( new THREE.RenderPass( scene, camera ) );
+  composer2 = new THREE.EffectComposer(renderer);
+  composer2.addPass(new THREE.RenderPass(scene, camera));
 
   shader = {
 
     uniforms: {
-      tDiffuse: { type: 't', value: null },
-      tColor: { type: 't', value: null },
-      resolution: { type: 'v2', value: new THREE.Vector2( 1, 1 ) },
-      viewProjectionInverseMatrix: { type: 'm4', value: new THREE.Matrix4() },
-      previousViewProjectionMatrix: { type: 'm4', value: new THREE.Matrix4() },
-      velocityFactor: { type: 'f', value: 1 }
+      tDiffuse: {type: 't', value: null},
+      tColor: {type: 't', value: null},
+      resolution: {type: 'v2', value: new THREE.Vector2(1, 1)},
+      viewProjectionInverseMatrix: {type: 'm4', value: new THREE.Matrix4()},
+      previousViewProjectionMatrix: {type: 'm4', value: new THREE.Matrix4() },
+      velocityFactor: {type: 'f', value: 1 }
     },
 
-    vertexShader: document.getElementById( 'vs-motionBlur' ).textContent,
-    fragmentShader: document.getElementById( 'fs-motionBlur' ).textContent
+    vertexShader: document.getElementById('vs-motionBlur').textContent,
+    fragmentShader: document.getElementById('fs-motionBlur').textContent
   }
 
-  pass = new THREE.ShaderPass( shader );
+  pass = new THREE.ShaderPass(shader);
   pass.renderToScreen = true;
-  composer.addPass( pass );
+  composer.addPass(pass);
 
-  container.appendChild( renderer.domElement );
+  container.appendChild(renderer.domElement);
 
-  window.addEventListener( 'resize', onWindowResize, false );
+  window.addEventListener('resize', onWindowResize, false);
   onWindowResize();
 
   var gui = new dat.GUI();
-  gui.add(params, 'blur', .1, 10 );
-  gui.add(params, 'fps', 1, 60 );
+  gui.add(params, 'blur', .1, 10);
+  gui.add(params, 'fps', 1, 60);
   gui.add(params, 'carSpeed', 0, 100);
 
 }
@@ -232,21 +227,19 @@ function init() {
 function onWindowResize() {
 
   var s = 1;
-  composer.setSize( s * window.innerWidth, s * window.innerHeight );
-  composer2.setSize( s * window.innerWidth, s * window.innerHeight );
+  composer.setSize(s * window.innerWidth, s * window.innerHeight);
+  composer2.setSize(s * window.innerWidth, s * window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( s * window.innerWidth, s * window.innerHeight );
-  pass.uniforms.resolution.value.set( s * window.innerWidth, s * window.innerHeight );
+  renderer.setSize(s * window.innerWidth, s * window.innerHeight);
+  pass.uniforms.resolution.value.set(s * window.innerWidth, s * window.innerHeight);
 
 }
-  
-//
 
 function animate() {
 
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
   render();
   
@@ -261,7 +254,7 @@ function render() {
   var ellapsedFactor = clock.getDelta();
 
   var t = Date.now();
-  if( t - lastTime > ( 1000 / params.fps ) ) {
+  if(t - lastTime > (1000 / params.fps)) {
 
     pass.material.uniforms.velocityFactor.value = params.blur;
 
@@ -269,19 +262,19 @@ function render() {
     scene.remove(mesh);
     car.position.x -= params.carSpeed;
     if(car.position.x < -3000) {
-      car.position.set(800, -260, 640);
+      car.position.set(1500, -260, 640);
     }
 
 
     camera.updateMatrix();
     camera.updateMatrixWorld();
 
-    tmpArray.copy( camera.matrixWorldInverse );
-    tmpArray.multiply( camera.projectionMatrix );
-    mCurrent.getInverse( tmpArray );
+    tmpArray.copy(camera.matrixWorldInverse);
+    tmpArray.multiply(camera.projectionMatrix);
+    mCurrent.getInverse(tmpArray);
 
-    pass.material.uniforms.viewProjectionInverseMatrix.value.copy( mCurrent );
-    pass.material.uniforms.previousViewProjectionMatrix.value.copy( mPrev );
+    pass.material.uniforms.viewProjectionInverseMatrix.value.copy(mCurrent);
+    pass.material.uniforms.previousViewProjectionMatrix.value.copy(mPrev);
     
     mesh.material = meshMaterial;
     mesh2.material = meshMaterial2;
@@ -292,13 +285,11 @@ function render() {
     pass.material.uniforms.tColor.value = composer2.renderTarget2;
     composer.render();
 
-    mPrev.copy( tmpArray );
+    mPrev.copy(tmpArray);
 
-    prevCamPos.copy( camera.position );
+    prevCamPos.copy(camera.position);
 
     lastTime = t;
 
   }
-  //renderer.render( scene, camera );
-
 }
